752459c9f1d7216a1bf9140871f4eae6d67dd65a
Fredrock21 <54800949+Fredrock21@users.noreply.github.com>
2019-09-09 13:08:25 +0200

    färdig

---
+++ V37/V37/letion1.java
@@ -0,0 +1,30 @@
+package V37;
+
+public class letion1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* en komntar här */
+		// en till komenar här
+		
+		int heltal1 = 5;// skapar en en låda som heter heltal1 och fyller den med ett hel tal
+		int svar; //deklarera
+		
+		svar = heltal1*heltal1;
+		
+		System.out.println("Svar: " + heltal1*heltal1); // + konkatinera
+			
+		double pi = 3.14;
+		double radie = 5;
+		
+		double area = pi*radie*radie;
+		
+		System.out.println("arean är:" + area + "cm^2");
+		
+		long stortHeltal = 5000000000000L;
+		
+		System.out.println(storHeltal + 1);
+
+	}
+
+}
---
+++ V37/V37/provasjelv.java
@@ -0,0 +1,15 @@
+package V37;
+
+public class provasjelv {
+	
+	public static void main(String[] args) {
+		
+		double pi = 3.14; 
+		double radie = 3;
+		
+		System.out.println((radie*radie*pi)/2);
+	
+	}
+
+
+}
