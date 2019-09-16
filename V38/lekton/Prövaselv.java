de2eddf49d9a133d9f930e87641892a53eae0b97
Fredrock21 <54800949+Fredrock21@users.noreply.github.com>
2019-09-16 16:50:12 +0200

    leton 3

---
+++ V38/lekton/Prövaselv.java
@@ -0,0 +1,16 @@
+package lekton;
+
+public class Prövaselv {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	
+		boolean age = true;
+		int svar = 0;
+		
+
+	}
+
+}
---
+++ V38/lekton/lekton3.java
@@ -0,0 +1,39 @@
+package lekton;
+
+import java.util.Scanner;
+
+public class lekton3 {
+
+	public static void main(String[] args) {
+		// villkor
+		boolean agein = true;
+		int svar = 1;
+		int age = 0; // initierat variabel
+
+		Scanner input = new Scanner(System.in); // gör det
+
+		if (agein) {
+
+			System.out.println("hur gammal är du");
+			age = input.nextInt();
+
+			if (age > 99) {
+				System.out.println("Gratulerar du får köra rumdraket");
+				System.out.println("plus bil och måped... men värkligen");
+			}
+
+			if (age >= 18 && age < 80) {
+
+				System.out.println("skafa bil och kör på");
+				System.out.println("du kan åka moped också");
+
+			} else if (age > 14 && age < 18) {
+				System.out.println("du kan köra måpe men inte bil");
+			} else {
+				System.out.println("nu får du inte köra något");
+			}
+
+		}
+	}
+
+}
