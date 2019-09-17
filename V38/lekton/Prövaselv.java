0b7502fc4fa41a9c205d57ac64c8a087b34b96fc
Fredrock21 <54800949+Fredrock21@users.noreply.github.com>
2019-09-17 11:28:55 +0200

    Update Prövaselv.java

--- V38/lekton/Prövaselv.java
+++ V38/lekton/Prövaselv.java
@@ -1,14 +1,15 @@
 package lekton;
 
+import java.util.Scanner;
+
 public class Prövaselv {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		
-	
-		boolean age = true;
 		int svar = 0;
+		Scanner input = new Scanner(System.in);
 		
 
 	}
