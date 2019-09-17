989d33e3e110b8cab643b0387037e5855e0d7846
Fredrock21 <54800949+Fredrock21@users.noreply.github.com>
2019-09-17 12:20:10 +0200

    lite försök.

---
+++ V38/lekton/lexsa2.java
@@ -0,0 +1,37 @@
+package lekton;
+
+import java.util.Scanner;
+
+public class lexsa2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int tal1;
+		int tal2;
+		int tal3;
+		int tal4;
+		int tal5;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("skriv ner 5 heltal mellan 1 och 9 utan desimaler, tack.");
+		
+		tal1 = input.nextInt();
+		tal2 = input.nextInt();
+		tal3 = input.nextInt();
+		tal4 = input.nextInt();
+		tal5 = input.nextInt();
+		
+		if (tal1 > tal2  tal3 || tal4 || tal5) { // skriv && istälet för|| så 
+			
+			System.out.println("\nHögst:\t");
+			System.out.println(tal1 +"");	
+		
+		}
+
+		}
+	
+	}
+
+
