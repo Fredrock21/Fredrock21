80216797b9d0b95980527cbf681f40ac58923d6d
Fredrock21 <54800949+Fredrock21@users.noreply.github.com>
2019-09-23 15:30:37 +0200

    Update lektion5.java

--- V39/V39/lektion5.java
+++ V39/V39/lektion5.java
@@ -1,10 +1,12 @@
 package V39;
 
+import java.util.Scanner;
+
 public class lektion5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
 			// Används när man vet exakt hur många gånger man vill repetera något
 			
 			// Skriver ut 0-4
@@ -21,7 +23,16 @@ public class lektion5 {
 			for (int i = 20 ; i > 0; i--) {
 				System.out.println(i);
 			}
+			System.out.println("skriv ett helatal under 10");
+			
+			int tal = input.nextInt();
+			int prudukt = 0 ;
+			
+			for (int i = 0 ; i <= 10 ;i++) {
+				prudukt = tal*i;
+				System.out.println(prudukt);
 
+				}
 			
 			}
 
