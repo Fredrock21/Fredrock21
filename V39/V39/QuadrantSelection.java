9d6753724947fee4427f21d1e873868c30d56af6
Fredrock21 <54800949+Fredrock21@users.noreply.github.com>
2019-09-24 19:48:16 +0200

    Update QuadrantSelection.java

--- V39/V39/QuadrantSelection.java
+++ V39/V39/QuadrantSelection.java
@@ -1,10 +1,29 @@
 package V39;
 
+import java.util.Scanner;
+
 public class QuadrantSelection {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		
+		int x = input.nextInt();
+		int y = input.nextInt();
+		
+		if(x>0 && y>0){
+			System.out.println("Quadrant 1");
+		}
+		if(x<0 && y>0){
+			System.out.println("Quadrant 2");
+		}
+		if(x<0 && y<0){
+			System.out.println("Quadrant 3");
+		}
+		if(x>0 && y<0){
+			System.out.println("Quadrant 4");
+		}
+		
 	}
 
 }
