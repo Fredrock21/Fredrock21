5b3f292c1dad609fae1b21310e6c1f4cd26eb207
Fredrock21 <54800949+Fredrock21@users.noreply.github.com>
2019-09-30 17:03:56 +0200

    Create TakeTwoStones.java

---
+++ V39/V39/TakeTwoStones.java
@@ -0,0 +1,15 @@
+package V39;
+
+import java.util.Scanner;
+
+public class TakeTwoStones {
+	Scanner input = new Scanner(System.in);
+
+	int N = input.nextInt();
+	
+	for (int i = N; i < N; i--) {
+		System.out.println(i);	
+		
+	}
+	
+}
