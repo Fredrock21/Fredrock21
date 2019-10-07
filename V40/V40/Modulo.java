6f8490b6d4a4cd9db5cb4d6e7f11b639d48d9c92
Fredrock21 <54800949+Fredrock21@users.noreply.github.com>
2019-10-07 11:42:21 +0200

    läxsa v40

---
+++ V40/V40/Modulo.java
@@ -0,0 +1,25 @@
+package V40;
+
+import java.util.Scanner;
+
+public class Modulo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner input = new Scanner(System.in);
+		
+		int tal1 = input.nextInt();
+		int tal2 = input.nextInt();
+		int tal3 = input.nextInt();
+		int tal4 = input.nextInt();
+		int tal5 = input.nextInt();
+		int tal6 = input.nextInt();
+		int tal7 = input.nextInt();
+		int tal8 = input.nextInt();
+		int tal9 = input.nextInt();
+		int tal10 = input.nextInt();
+		
+	
+	}
+
+}
