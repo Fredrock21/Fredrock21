34ad47ae24f49cade7e00785cdecea4ca80e7051
Fredrock21 <54800949+Fredrock21@users.noreply.github.com>
2020-03-02 12:49:32 +0100

    Update gisningslek.java

--- V9/gisningslek.java
+++ V9/gisningslek.java
@@ -3,188 +3,194 @@ import java.util.Scanner;
 
 public class gisningslek {
 	
+	static Scanner input = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		
-		Scanner input = new Scanner(System.in);
-		
-		System.out.println("I deta spelet ska du gisa ett tall mellan några interval du får 5 försök på dig! Välj svårighets grad");
-		System.out.println("[1] lätt 1-10");
-		System.out.println("[2] mellan 1-100");
-		System.out.println("[3] svår 1-1000");
-		System.out.println();
-		
-		int choice;
-		
-		choice = input.nextInt();
- 
-		switch (choice) {
-		case 1:
-			System.out.println("du valde lätt");
-			long lätt = Math.round((Math.random()*10));
-			
-			System.out.println(lättaste(lätt));
-			
-			break;
- 
-		case 2:
-			System.out.println("du valde mellan");
-			long medel = Math.round((Math.random()*100));
-			
-			System.out.println(mellan(medel));
- 
-			break;
- 
-		case 3:
- 
-			System.out.println("du valde svår");
-			long svår = Math.round((Math.random()*1000));
-			
-			System.out.println(svåraste(svår));
-			
-			break;
- 
-		default:
-			
-			System.out.println("du har inte valt en svårighetsgard :(");
-			
-			break;
-			
+	public static void main(String[] args) {		
+
+		System.out.println(
+				"I deta spelet ska du gisa ett tall mellan några interval du får 5 försök på dig! Välj svårighets grad");
+		boolean success = false;
+		while (!success) {
+
+			System.out.println("[1] lätt 1-10");
+			System.out.println("[2] mellan 1-100");
+			System.out.println("[3] svår 1-1000");
+			System.out.println();
+
+			int choice;
+
+			choice = input.nextInt();
+
+			switch (choice) {
+			case 1:
+				System.out.println("du valde lätt");
+				long lätt = Math.round((Math.random() * 10));
+
+				lättaste(lätt);
+
+				break;
+
+			case 2:
+				System.out.println("du valde mellan");
+				long medel = Math.round((Math.random() * 100));
+
+				mellan(medel);
+
+				break;
+
+			case 3:
+
+				System.out.println("du valde svår");
+				long svår = Math.round((Math.random() * 1000));
+
+				svåraste(svår);
+
+				break;
+
+			default:
+
+				System.out.println("du har inte valt en svårighetsgard :(");
+
+				break;
+
+			}
+			String J;
+			System.out.println("vill du spela i gen?");
+			System.out.println("ja eller nej");
+
+			J = input.nextLine();
+
+			if (J.equals("ja")) {
+				System.out.println("bra");
+
+			} else {
+				System.out.println("okej");
+				success = true;
+			}
 		}
 	}
+
 	public static void lättaste(long lätt) {
-		
 
 		int svar;
-		
+
 		int gisnigar = 5;
-		
+
 		boolean exit = false;
-		
-		while (!exit){
-		
+
+		while (!exit) {
+
 			svar = input.nextInt();
-		
-		if (svar == lätt) {
-			exit = true;
-			System.out.println("du har rett de arr " + lätt);
-			
+
+			if (svar == lätt) {
+				exit = true;
+				System.out.println("du har rett de svaret var " + lätt);
+
+			}
+
+			else if (svar > lätt) {
+
+				gisnigar--;
+
+				System.out.println("lägre du har " + gisnigar + " gisnigar kvar");
+
+			} else if (svar < lätt) {
+
+				gisnigar--;
+
+				System.out.println("högre du har " + gisnigar + " gisnigar kvar");
+
+			}
+			if (gisnigar == 0) {
+
+				exit = true;
+				System.out.println("du mislykades inga gisnaigar kavr");
+			}
+
 		}
-		
-		else if (svar > lätt) {
-			
-			gisnigar--;
-			
-			System.out.println("lägre du har " + gisnigar + " gisnigar kvar");
-			
-		}
-		else if (svar < lätt) {
-			
-			gisnigar--;
-			
-			System.out.println("högre du har " + gisnigar + " gisnigar kvar");
-			
-		}
-		if (gisnigar == 0) {
-			
-			exit = true;
-			System.out.println("du mislykades inga gisnaigar kavr");
-		}
-			
-			
-		}
-		
-		
 
 	}
-	
-	
+
 	public static void mellan(long medel) {
-		
-	int svar;
-		
-		int gisnigar = 5;
-		
-		boolean exit = false;
-		
-		while (!exit){
-		
-			svar = input.nextInt();
-		
-		if (svar == medel) {
-			exit = true;
-			System.out.println("du har rett de arr " + lätt);
-			
-		}
-		
-		else if (svar > medel) {
-			
-			gisnigar--;
-			
-			System.out.println("lägre du har " + gisnigar + " gisnigar kvar");
-			
-		}
-		else if (svar < medel) {
-			
-			gisnigar--;
-			
-			System.out.println("högre du har " + gisnigar + " gisnigar kvar");
-			
-		}
-		if (gisnigar == 0) {
-			
-			exit = true;
-			System.out.println("du mislykades inga gisnaigar kavr");
-		}
-			
-			
-		}
-		
-	}
-	
-	public static void svåraste(long svår) {
-		
+
 		int svar;
-		
+
 		int gisnigar = 5;
-		
+
 		boolean exit = false;
-		
-		while (!exit){
-		
+
+		while (!exit) {
+
 			svar = input.nextInt();
-		
-		if (svar == svår) {
-			exit = true;
-			System.out.println("du har rett de arr " + lätt);
-			
+
+			if (svar == medel) {
+				exit = true;
+				System.out.println("du har rett de svaret var " + medel);
+
+			}
+
+			else if (svar > medel) {
+
+				gisnigar--;
+
+				System.out.println("lägre du har " + gisnigar + " gisnigar kvar");
+
+			} else if (svar < medel) {
+
+				gisnigar--;
+
+				System.out.println("högre du har " + gisnigar + " gisnigar kvar");
+
+			}
+			if (gisnigar == 0) {
+
+				exit = true;
+				System.out.println("du mislykades inga gisnaigar kavr");
+			}
+
 		}
-		
-		else if (svar > svår) {
-			
-			gisnigar--;
-			
-			System.out.println("lägre du har " + gisnigar + " gisnigar kvar");
-			
-		}
-		else if (svar < svår) {
-			
-			gisnigar--;
-			
-			System.out.println("högre du har " + gisnigar + " gisnigar kvar");
-			
-		}
-		if (gisnigar == 0) {
-			
-			exit = true;
-			System.out.println("du mislykades inga gisnaigar kavr");
-		}
-			
-			
-		}
-		
+
 	}
 
+	public static void svåraste(long svår) {
 
+		int svar;
+
+		int gisnigar = 5;
+
+		boolean exit = false;
+
+		while (!exit) {
+
+			svar = input.nextInt();
+
+			if (svar == svår) {
+				exit = true;
+				System.out.println("du har rett svaret var " + svår);
+
+			}
+
+			else if (svar > svår) {
+
+				gisnigar--;
+
+				System.out.println("lägre du har " + gisnigar + " gisnigar kvar");
+
+			} else if (svar < svår) {
+
+				gisnigar--;
+
+				System.out.println("högre du har " + gisnigar + " gisnigar kvar");
+
+			}
+			if (gisnigar == 0) {
+
+				exit = true;
+				System.out.println("du mislykades inga gisnaigar kavr");
+			}
+
+		}
+
+	}
 
 }
